c58433b8e9e22eb1acb352daa02fd7b2499559fa
Adanos020 <adanos020@gmail.com>
2022-02-01 14:06:19 +0000

    Create window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use winit::{
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::ControlFlow;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .with_title("ErupTrace")
+        .build(&event_loop)
+        .expect("Could not create window.");
+
+    event_loop.run(|event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::MainEventsCleared => {
+                // TODO: Render here
+            }
+            _ => {}
+        }
+    });
 }
