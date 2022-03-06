c42288742f51b0247923f98e29fef98c3ed68f00
Adanos020 <adanos020@gmail.com>
2022-03-06 01:07:30 +0000

    Replace hard-coded camera definition with JSON files in scenes.

--- eruptrace_scene/src/lib.rs
+++ eruptrace_scene/src/lib.rs
@@ -3,7 +3,8 @@ pub mod json;
 pub mod materials;
 pub mod shapes;
 
-use crate::{materials::*, shapes::*};
+use crate::json::to_vec3;
+use crate::{camera::Camera, materials::*, shapes::*};
 use serde_json as js;
 use std::{
     fs,
@@ -18,70 +19,99 @@ pub struct Scene {
 }
 
 impl Scene {
-    pub fn load<P: AsRef<Path>>(scene_path: P) -> anyhow::Result<Self> {
-        let mut desc_path = PathBuf::new();
-        desc_path.push(&scene_path);
-        desc_path.push("scene.json");
-        let file_contents = fs::read_to_string(desc_path)?;
-        let scene_json: js::Value = js::from_str(&file_contents)?;
+    pub fn load<P: AsRef<Path>>(scene_path: P) -> anyhow::Result<(Camera, Self)> {
+        let camera = {
+            let mut cam_path = PathBuf::new();
+            cam_path.push(&scene_path);
+            cam_path.push("camera.json");
+            let file_contents = fs::read_to_string(cam_path)?;
+            let cam_json: js::Value = js::from_str(&file_contents)?;
 
-        let (texture_names, texture_paths) = {
-            let obj = scene_json["textures"].as_object().unwrap();
-            let mut names: Vec<String> = obj.keys().map(|n| n.to_owned()).collect();
-            let mut paths: Vec<PathBuf> = obj
-                .values()
-                .filter(|p| p.is_string())
-                .map(|p| {
-                    let mut tex_path = PathBuf::new();
-                    tex_path.push(&scene_path);
-                    tex_path.push("textures");
-                    tex_path.push(p.as_str().unwrap());
-                    tex_path
-                })
-                .collect();
-            if let Some(sky_idx) = names.iter().position(|n| n == "sky") {
-                unsafe {
-                    std::ptr::swap(&mut paths[0], &mut paths[sky_idx]);
-                    std::ptr::swap(&mut names[0], &mut names[sky_idx]);
-                }
-            } else {
-                eprintln!("Missing 'sky' texture.");
+            let position = to_vec3(&cam_json["position"]);
+            let look_at = to_vec3(&cam_json["look_at"]);
+            let up = to_vec3(&cam_json["up"]);
+            let vertical_fov = cam_json["fov"].as_f64().unwrap_or(90.0) as f32;
+            let samples = cam_json["samples"].as_u64().unwrap_or(1) as u32;
+            let max_reflections = cam_json["max_reflections"].as_u64().unwrap_or(1) as u32;
+
+            Camera {
+                position,
+                look_at,
+                up,
+                img_size: [0, 0],
+                vertical_fov,
+                samples,
+                max_reflections,
             }
-            (names, paths)
         };
 
-        let (material_names, materials) = {
-            let obj = scene_json["materials"].as_object().unwrap();
-            let names: Vec<String> = obj.keys().map(|n| n.to_owned()).collect();
-            let materials: Vec<Material> = obj
-                .values()
-                .filter(|m| m.is_object())
-                .map(|m| Material::from_json(m, &texture_names).unwrap())
+        let scene = {
+            let mut desc_path = PathBuf::new();
+            desc_path.push(&scene_path);
+            desc_path.push("scene.json");
+            let file_contents = fs::read_to_string(desc_path)?;
+            let scene_json: js::Value = js::from_str(&file_contents)?;
+
+            let (texture_names, texture_paths) = {
+                let obj = scene_json["textures"].as_object().unwrap();
+                let mut names: Vec<String> = obj.keys().map(|n| n.to_owned()).collect();
+                let mut paths: Vec<PathBuf> = obj
+                    .values()
+                    .filter(|p| p.is_string())
+                    .map(|p| {
+                        let mut tex_path = PathBuf::new();
+                        tex_path.push(&scene_path);
+                        tex_path.push("textures");
+                        tex_path.push(p.as_str().unwrap());
+                        tex_path
+                    })
+                    .collect();
+                if let Some(sky_idx) = names.iter().position(|n| n == "sky") {
+                    unsafe {
+                        std::ptr::swap(&mut paths[0], &mut paths[sky_idx]);
+                        std::ptr::swap(&mut names[0], &mut names[sky_idx]);
+                    }
+                } else {
+                    eprintln!("Missing 'sky' texture.");
+                }
+                (names, paths)
+            };
+
+            let (material_names, materials) = {
+                let obj = scene_json["materials"].as_object().unwrap();
+                let names: Vec<String> = obj.keys().map(|n| n.to_owned()).collect();
+                let materials: Vec<Material> = obj
+                    .values()
+                    .filter(|m| m.is_object())
+                    .map(|m| Material::from_json(m, &texture_names).unwrap())
+                    .collect();
+                (names, materials)
+            };
+
+            let spheres = scene_json["spheres"]
+                .as_array()
+                .map_or(&vec![], |v| v)
+                .iter()
+                .filter(|s| s.is_object())
+                .map(|s| Sphere::from_json(s, &material_names).unwrap())
                 .collect();
-            (names, materials)
+
+            let meshes = scene_json["meshes"]
+                .as_array()
+                .map_or(&vec![], |v| v)
+                .iter()
+                .filter(|m| m.is_object())
+                .map(|m| Mesh::from_json(m, &material_names).unwrap())
+                .collect();
+
+            Self {
+                spheres,
+                meshes,
+                materials,
+                texture_paths,
+            }
         };
 
-        let spheres = scene_json["spheres"]
-            .as_array()
-            .map_or(&vec![], |v| v)
-            .iter()
-            .filter(|s| s.is_object())
-            .map(|s| Sphere::from_json(s, &material_names).unwrap())
-            .collect();
-
-        let meshes = scene_json["meshes"]
-            .as_array()
-            .map_or(&vec![], |v| v)
-            .iter()
-            .filter(|m| m.is_object())
-            .map(|m| Mesh::from_json(m, &material_names).unwrap())
-            .collect();
-
-        Ok(Self {
-            spheres,
-            meshes,
-            materials,
-            texture_paths,
-        })
+        Ok((camera, scene))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use eruptrace_pure::run_app;
-use eruptrace_scene::{camera::Camera, Scene};
-use nalgebra_glm as glm;
+use eruptrace_scene::Scene;
 use std::path::PathBuf;
 
 struct EruptraceArgs {
@@ -22,16 +21,7 @@ impl EruptraceArgs {
 fn main() {
     match EruptraceArgs::parse_args() {
         Ok(args) => {
-            let camera = Camera {
-                position: glm::vec3(1.5, 1.5, 1.5),
-                look_at: glm::vec3(0.5, 0.5, 0.5),
-                up: glm::vec3(0.0, 1.0, 0.0),
-                vertical_fov: 90.0,
-                img_size: [0, 0],
-                samples: 50,
-                max_reflections: 10,
-            };
-            let scene = Scene::load(args.scene_path).unwrap();
+            let (camera, scene) = Scene::load(args.scene_path).unwrap();
             run_app(camera, scene);
         }
         Err(e) => {
